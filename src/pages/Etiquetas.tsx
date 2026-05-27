b93262b6b3324d301f41edd347c955406c11acbc
Filipinas0990 <filipeegustavo0990@gmail.com>
2026-05-26 21:25:56 -0300

    Fe em deus que agora foi

--- src/pages/Etiquetas.tsx
+++ src/pages/Etiquetas.tsx
@@ -333,7 +333,8 @@ export default function Etiquetas() {
     <div className="min-h-screen bg-background">
       <Sidebar />
 
-      <main className="md:ml-16 p-4 md:p-8 pb-24 md:pb-8 space-y-6 max-w-5xl mx-auto">
+      <main className="md:ml-16 min-h-screen">
+        <div className="max-w-4xl mx-auto p-4 md:p-8 pb-24 md:pb-8 space-y-6">
         {/* Voltar */}
         <button
           onClick={() => navigate("/dashboard/configuracoes")}
@@ -506,6 +507,7 @@ export default function Etiquetas() {
             })}
           </div>
         )}
+        </div>
       </main>
 
       {/* Modal criar/editar */}
